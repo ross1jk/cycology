c67aaa6d4152c3355cff6db5feda21f78f2d28aa
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-16 10:50:59 -0500

    search button function started for on click event

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function(){
+
+/*ids needed for getting local storage:
+searchBtn
+inputStart
+inputEnd
+weather
+*/
+var searchedRoute
+var savedRoute 
+
+$("#searchBtn").click(function() {
+   let start = $("#inputStart").val().trim(); 
+   let end = $("#inputEnd").val().trim();
+   let weather = $("#weather").val().trim();
+
+  });
+
+
+
+
+
+//end of start function 
+}); 
