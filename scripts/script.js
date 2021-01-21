42dfea2adf464b04d7bc4006de2885f01754c9da
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-20 20:01:24 -0500

    map api pulling a query

--- scripts/script.js
+++ scripts/script.js
@@ -7,19 +7,19 @@ inputEnd
 weather
 */
 
-$("#searchBtn").click(function() {
-   let start= $("#inputStart").val().trim();
-   let end= $("#inputEnd").val().trim();
-   let weather= $("#weather").val().trim()
-   localStorage.setItem("start", start); 
-   localStorage.setItem("end", end); 
-   localStorage.setItem("weater", weather)
- });
+// $("#searchBtn").click(function() {
+//    let start= $("#inputStart").val().trim();
+//    let end= $("#inputEnd").val().trim();
+//    let weather= $("#weather").val().trim()
+//    localStorage.setItem("start", start); 
+//    localStorage.setItem("end", end); 
+//    localStorage.setItem("weater", weather)
+//  });
 
 //geocode to get longitutde and latitude 
 //function findCoordinates(){
-let getstart = localStorage.getItem("start");
-let getend =  localStorage.getItem("end")
+let getstart = "Gardenia Ave, Royal Oak MI" //localStorage.getItem("start");
+let getend =  "Main St., Royal Oak MI" //localStorage.getItem("end")
 let startCord = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + getstart + ".json?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
 let endCord = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + getend + ".json?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
 
@@ -28,22 +28,36 @@ let endCord = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + getend + ".
    method: "GET"
  }).then(function(response){
     //I believe this is the right order for lat and lon? 
-   let startlat = response.features[0].geometry.coordinates[0];
-   let startlon = response.features[0].geometry.coordinates[1]; 
-   console.log(start + ": " + startlat + " , " + startlon);
-   localStorage.setItem("endCord", startlat + "," + startlon); 
+   let startlon = response.features[0].geometry.coordinates[0];
+   let startlat = response.features[0].geometry.coordinates[1]; 
+   console.log("Start" + startlon + "," + startlat);
+   localStorage.setItem("startCord", startlon + "," + startlat); 
  });
 
 $.ajax({
   url: endCord, 
   method: "GET"
 }).then(function(response){
-  let endlat = response.features[0].geometry.coordinates[0];
   let endlon = response.features[0].geometry.coordinates[0];
-  console.log(end + ": " + endlat + " , " + endlon); 
-  localStorage.setItem("startCord", endlat + "," + endlon); 
+  let endlat = response.features[0].geometry.coordinates[1];
+  console.log("end: " + endlon + " , " + endlat); 
+  localStorage.setItem("endCord", endlon + "," + endlat); 
 });
 
+//mapbox api
+//function currentRoute(){
+let startCordLS = localStorage.getItem("startCord"); 
+let endCordLS = localStorage.getItem("endCord"); 
+console.log(startCordLS + " " + endCordLS);
+routeURL = "https://api.mapbox.com/directions/v5/mapbox/cycling/" + startCordLS + ";" + endCordLS + "?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
+$.ajax({
+  url: routeURL, 
+  method: "GET"
+}).then(function(response){
+//displaycode here
+console.log(response); 
+}); 
 
+//}
 //end of start function 
 }); 
