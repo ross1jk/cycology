7e0075a84777784f0a29f5f90ef9242a88a0f4af
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-24 10:24:45 -0400

    added seed inforation of what will be saved

---
+++ routeseed.sql
@@ -0,0 +1,5 @@
+-- Table will be called route -- 
+-- user entries will be start and end locations, route rating & comments; eventually would like this to be done uniquely by user -- 
+-- example entries--
+INSERT INTO route (start_location, end_location, route_rating, comments)
+VALUES ('Royal Oak MI', 'Goodrich MI', 4, 'too long'), ('Detroit MI', 'Royal Oak MI', 6, 'kinda scary but fun'), ('1707 Gardenia Ave Royal Oak MI', 'Main St Royal Oak MI', 3, 'way too short'); 
