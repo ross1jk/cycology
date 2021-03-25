55606bbc6b3b84332b2c93129eeb7d0eb1325865
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-24 21:05:12 -0400

    all pages in html routes are rendering

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,4 +5,12 @@ module.exports = (app) => {
     app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/search-element.html'))
     ); 
-}
+
+    app.get('/searchedroute',(req, res) =>
+    res.sendFile(path.join(__dirname, '../public/ResultsPage.html'))
+    );
+
+    app.get('/savedroutes',(req, res) =>
+    res.sendFile(path.join(__dirname, '../public/savedroutes.html'))
+    );
+}; 
