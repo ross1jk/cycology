ee60119960d28da466b88cf4a7f8047ae0393314
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-26 18:40:59 -0400

    index renders

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = (app) => {
     //homepage
     app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/search-element.html'))
+    res.render("index")
     ); 
 
     app.get('/searchedroute',(req, res) =>
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-
+const exphbs = require("express-handlebars");
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 const db = require('./models/cycleroute');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 app.use(express.static('/public'));
 
 // require('./app/routes/api-routes.js')(app);
