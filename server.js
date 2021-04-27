f239d590f082c34fb521de001b0d4f339ca46700
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-27 10:25:53 -0400

    saving each entry now in db

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ htmlRouter(app);
 apiRouter(app);
 
 // Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
