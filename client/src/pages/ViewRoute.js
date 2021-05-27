fdf3e09447f41beed690a0733e6c5398e83e8d5a
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-05-27 14:09:56 -0400

    fixed lat and lon storage to store in correct order

--- client/src/pages/ViewRoute.js
+++ client/src/pages/ViewRoute.js
@@ -29,17 +29,27 @@ function ViewRoute() {
         let end = data.end_location; 
         API.findCoords(start)
         .then(res => 
-           { setStartCoordinates({ latStart: res.data.features[0].center[0], lonStart: res.data.features[0].center[1] })
-           axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + res.data.features[0].center[0] + "&lon=" + res.data.features[0].center[1] + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")
-            .then(res => setWeatherQuery(res.data))
+           { setStartCoordinates({ latStart: res.data.features[0].geometry.coordinates[1], lonStart: res.data.features[0].geometry.coordinates[0] })
+           axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + res.data.features[0].geometry.coordinates[1] + "&lon=" + res.data.features[0].geometry.coordinates[0] + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")
+            .then(res => setWeatherQuery(res.data.daily))
         })
         API.findCoords(end)
-        .then(res => setEndCoordinates({ latEnd: res.data.features[0].center[0], lonEnd: res.data.features[0].center[1] }))
+        .then(res => setEndCoordinates({ latEnd: res.data.features[0].geometry.coordinates[1], lonEnd: res.data.features[0].geometry.coordinates[0] }))
     }
 
     return (
         <div>
            {console.log(weather)}
+           {console.log("https://api.openweathermap.org/data/2.5/onecall?lat=" +startCoordinates.lonStart + "&lon=" + startCoordinates.latStart + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")}
+           {weather.map((temp, index) =>
+           <Card 
+               key={index}
+               id={temp[index]}
+               cardTitleOne={temp.temp.day}
+               cardContent={temp.weather[0].main}
+           />
+           )}
+
             <Card
                 id={currentRoute._id}
                 cardTitleOne={currentRoute.start_location}
@@ -47,6 +57,7 @@ function ViewRoute() {
                 to="to"
                 cardContent={currentRoute.review}
             />
+
         </div>
     )
 }
