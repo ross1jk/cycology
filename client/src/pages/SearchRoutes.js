aa0ca6e16fe6f9fa8f50b21294005f5d05a8e65b
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-07-28 09:14:54 -0400

    map loading without errors

--- client/src/pages/SearchRoutes.js
+++ client/src/pages/SearchRoutes.js
@@ -12,8 +12,15 @@ function Search() {
         )
     const [currentRoute, setCurrent] = useState([]);
     const [routeInstructions, setInstructions] = useState([]);
-    const [startCoordinates, setStart] = useState([]);
-    const [endCoordinates, setEnd] = useState([]);
+    const [startCoordinates, setStart] = useState({
+        lonStart: 83.1446,
+        latStart: 42.4895,
+    });
+    const [endCoordinates, setEnd] = useState({
+        lonEnd: 83.0458,
+        latEnd: 42.3314,
+    });
+    
     const [weather, setWeatherQuery] = useState([]);
 
     function handleInputChange(event) {
@@ -41,7 +48,19 @@ function Search() {
                 axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + res.data.features[0].geometry.coordinates[1] + "&lon=" + res.data.features[0].geometry.coordinates[0] + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")
                     .then(res => setWeatherQuery(res.data.daily))
             })
+
+            // let start = startCoordinates.lonStart + "," + startCoordinates.latStart
+            // let end = endCoordinates.lonEnd + "," + endCoordinates.latEnd
+
+            axios.get("https://api.mapbox.com/directions/v5/mapbox/cycling/" + startCoordinates.lonStart + "," + startCoordinates.latStart + ";" + endCoordinates.lonEnd + "," + endCoordinates.latEnd + "?steps=true&access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A")
+            .then(response => {
+                setInstructions(response.data.routes[0].legs[0].steps)
+            })
+    
     }
+
+    let map = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+9ed4bd(" + startCoordinates.lonStart + "," + startCoordinates.latStart + "),pin-s-b+000(" + endCoordinates.lonEnd + "," + endCoordinates.latEnd + ")/auto/500x300?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A";        
+
     function handleFormSubmit(event) {
         event.preventDefault(); 
         API.findCoords(route.start_location)
@@ -61,17 +80,6 @@ function Search() {
             })
 
     };
-
-    let start = startCoordinates.lonStart + "," + startCoordinates.latStart
-    let end = endCoordinates.lonEnd + "," + endCoordinates.latEnd
-    let map = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+9ed4bd(" + start + "),pin-s-b+000(" + end + ")/auto/500x300?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
-
-
-    axios.get("https://api.mapbox.com/directions/v5/mapbox/cycling/" + start + ";" + end + "?steps=true&access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A")
-        .then(response => {
-            setInstructions(response.data.routes[0].legs[0].steps)
-        }
-        )
 
     function Save(event){
         event.preventDefault();
