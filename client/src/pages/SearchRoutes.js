da42a23b8436a1742b08e7c937a235dbe4c1ab95
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-07-28 10:33:55 -0400

    updated pages to remove errors

--- client/src/pages/SearchRoutes.js
+++ client/src/pages/SearchRoutes.js
@@ -1,51 +1,177 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../components/Input";
 import API from "../utils/API";
-import { Container } from "../components/Grid";
+import { Container, Row, Col } from "../components/Grid";
+import Card from "../components/Card";
+import axios from "axios";
 import { createBrowserHistory } from "history";
 
 function Search() {
-    const [route, setRoute] = useState([])
+    const [route, setRoute] = useState(
+        {
+            start_location: "Royal Oak Michigan",
+            end_location: "Detroit Michigan"
+        }
+    )
+    const [currentRoute, setCurrent] = useState([]);
+    // const [routeInstructions, setInstructions] = useState([]);
+    const [startCoordinates, setStart] = useState({
+        lonStart: 83.1446,
+        latStart: 42.4895,
+    });
+    const [endCoordinates, setEnd] = useState({
+        lonEnd: 83.0458,
+        latEnd: 42.3314,
+    });
     const history = createBrowserHistory({ forceRefresh: true });
+
+    const [weather, setWeatherQuery] = useState([]);
+
+    useEffect(() => {
+        pageLoad()
+    }, [])
 
     function handleInputChange(event) {
         const { name, value } = event.target;
         setRoute({ ...route, [name]: value })
     }
 
+    function pageLoad() {
+        API.findCoords(route.start_location)
+            .then(res => setStart({
+                latStart: res.data.features[0].geometry.coordinates[1],
+                lonStart: res.data.features[0].geometry.coordinates[0]
+            }))
+        API.findCoords(route.end_location)
+            .then(res => setEnd({
+                latEnd: res.data.features[0].geometry.coordinates[1],
+                lonEnd: res.data.features[0].geometry.coordinates[0]
+            }))
+        API.findCoords(route.start_location)
+            .then(res => {
+                axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + res.data.features[0].geometry.coordinates[1] + "&lon=" + res.data.features[0].geometry.coordinates[0] + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")
+                    .then(res => setWeatherQuery(res.data.daily))
+            })
+    }
+
+    let map = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+9ed4bd(" + startCoordinates.lonStart + "," + startCoordinates.latStart + "),pin-s-b+000(" + endCoordinates.lonEnd + "," + endCoordinates.latEnd + ")/auto/500x300?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A";
+
+    // route instructions are forcing a time out
+
+    // let start = startCoordinates.lonStart + "," + startCoordinates.latStart
+    // let end = endCoordinates.lonEnd + "," + endCoordinates.latEnd    
+
+    // axios.get("https://api.mapbox.com/directions/v5/mapbox/cycling/" + startCoordinates.lonStart + "," + startCoordinates.latStart + ";" + endCoordinates.lonEnd + "," + endCoordinates.latEnd + "?steps=true&access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A")
+    // .then(response => {
+    //     console.log(response)
+    //     // setInstructions(response.data.routes[0].legs[0].steps)
+    // })
+
+
     function handleFormSubmit(event) {
         event.preventDefault();
-            API.createRoutes({
-            start_location: route.start_location,
-            end_location: route.end_location,           
-        }).then(res => history.push("/viewroute/" + res.data._id))
-        .catch(err => console.log(err))
+        API.findCoords(route.start_location)
+            .then(res => setStart({
+                latStart: res.data.features[0].geometry.coordinates[1],
+                lonStart: res.data.features[0].geometry.coordinates[0]
+            }))
+        API.findCoords(route.end_location)
+            .then(res => setEnd({
+                latEnd: res.data.features[0].geometry.coordinates[1],
+                lonEnd: res.data.features[0].geometry.coordinates[0]
+            }))
+
+        API.findCoords(route.start_location)
+            .then(res => {
+                axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + res.data.features[0].geometry.coordinates[1] + "&lon=" + res.data.features[0].geometry.coordinates[0] + "&units=imperial&appid=101220419d85ffb610459f1145df78ff")
+                    .then(res => setWeatherQuery(res.data.daily))
+            })
+
     };
+
+    function Save(event) {
+        event.preventDefault();
+        API.createRoutes({
+            start_location: route.start_location,
+            end_location: route.end_location,
+            start_coordinates: startCoordinates.lonStart + "," + startCoordinates.latStart,
+            end_coordinates: endCoordinates.lonEnd + "," + endCoordinates.latEnd
+        })
+            .catch(err => console.log(err))
+            .then(history.push(`/saved/`) //if pass in unique ids this would have to be a param eventually
+            )
+    }
 
     return (
         <div>
             <Container>
-            <div className="card" id="search">
-                <div className="card-body">
-                <form id="newRoute" onSubmit={handleFormSubmit}>
-                    <Input
-                        value={route.start_location}
-                        onChange={handleInputChange}
-                        type="text"
-                        name="start_location"
-                        placeholder="Start: Road, City, State"
-                    />
-                    <Input
-                        value={route.end_location}
-                        onChange={handleInputChange}
-                        type="text"
-                        name="end_location"
-                        placeholder="End: Road, City, State"
-                    />
-                    <button type="submit" className="btn btn-light" id="submit">Search Routes</button>
-                </form>
+                <div className="card" id="search">
+                    <div className="card-body">
+                        <form id="newRoute" onSubmit={handleFormSubmit}>
+                            <Input
+                                value={route.start_location}
+                                onChange={handleInputChange}
+                                type="text"
+                                name="start_location"
+                                placeholder="Start: Road, City, State"
+                            />
+                            <Input
+                                value={route.end_location}
+                                onChange={handleInputChange}
+                                type="text"
+                                name="end_location"
+                                placeholder="End: Road, City, State"
+                            />
+                            <button type="submit" className="btn btn-light" id="submit">Search Routes</button>
+                        </form>
+                    </div>
                 </div>
-                </div>
+
+                <Row>
+                    <Card
+                        key="location"
+                        id={currentRoute._id}
+                        cardTitleOne={route.start_location}
+                        cardTitleTwo={route.end_location}
+                        to="to"
+                    />
+                    <button type="submit" className="btn btn-light" onClick={Save} id="submit">Save This Route</button>
+
+                </Row>
+                <Row>
+                    <Col size="6">
+                        <div>
+                            <h2 className="view">Route</h2>
+                            <img src={map} alt="route"></img>
+                        </div>
+                    </Col>
+                    <Col size="6">
+                        {/*<div id="routeInstructions"> 
+               {/* <h2 className="view">Route Instructions</h2>
+                {/* do an or in case this doenst load */}
+                        {/* {routeInstructions.map((route, index) =>
+                        <li key={index}>
+                            {route.maneuver.instruction}
+                        </li>
+                    )} */}
+                        {/* </div> */}
+                        <Row>
+                            <h2 className="view">8 Day Forecast</h2>
+                            {weather.map((temp) =>
+                                <Col size="4" key={temp.dt}>
+                                    <Card
+                                        key={temp.dt}
+                                        cardTitleOne={temp.temp.day}
+                                        to="°F"
+                                        cardContent={temp.weather[0].main}
+                                    />
+                                </Col>
+                            )}
+                        </Row>
+                    </Col>
+                </Row>
+
+
             </Container>
         </div>
     )
--- client/src/pages/ViewRoute.js
+++ client/src/pages/ViewRoute.js
@@ -15,33 +15,17 @@ function ViewRoute() {
 
     useEffect(() => {
         loadRoutes(id)
-    }, [])
+    }, [id])
 
     function loadRoutes(id) {
         API.findOne(id)
             .then(res => {
                 setCurrent(res.data)
                 findWeather(res.data)
-                findCoords(res.data)
-            }).catch(err => console.log(err));
-    }
+                routeDetails(res.data)
 
-    // if (loadRoutes){
-    //     otherData()
-    // }
-
-    function findCoords(data) {
-        API.findCoords(data.start_location)
-            .then(res => setStart({
-                latStart: res.data.features[0].geometry.coordinates[1],
-                lonStart: res.data.features[0].geometry.coordinates[0]
-            }))
-
-        API.findCoords(data.end_location)
-            .then(res => setEnd({
-                latEnd: res.data.features[0].geometry.coordinates[1],
-                lonEnd: res.data.features[0].geometry.coordinates[0]
-            }))
+            })
+            .catch(err => console.log(err));
     }
 
     function findWeather(data) {
@@ -53,54 +37,50 @@ function ViewRoute() {
             })
     }
 
-    function routeDetails(){
-        let start = startCoordinates.lonStart + "," + startCoordinates.latStart
-        let end = endCoordinates.lonEnd + "," + endCoordinates.latEnd
-       // let map = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+9ed4bd(" + start + "),pin-s-b+000(" + end + ")/auto/500x300?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
+console.log(currentRoute)
+let map = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s-a+9ed4bd(" + currentRoute.start_coordinates + "),pin-s-b+000(" + currentRoute.end_coordinates + ")/auto/500x300?access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A"
 
-        axios.get("https://api.mapbox.com/directions/v5/mapbox/cycling/" + start + ";" + end + "?steps=true&access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A")
-            .then(response => {
-                setInstructions(response.data.routes[0].legs[0].steps)
-            }
-            )
-      //  document.getElementById("displayMap").append(<img src={map} alt="route"></img>)
-    }
-
+function routeDetails(data) {
+    axios.get("https://api.mapbox.com/directions/v5/mapbox/cycling/" + data.start_coordinates + ";" + data.end_coordinates + "?steps=true&access_token=pk.eyJ1Ijoicm9zczFqayIsImEiOiJja2p0YzJ0bmowOTd3MnFxc2c0Z2NiMWw0In0.3mcyR7CpPBKi_sGyVdA26A")
+    .then(response => { 
+        setInstructions(response.data.routes[0].legs[0].steps)
+    })
+}
     return (
         <div>
             <Container>
+            <Row>
+            <Card
+                    key="location"
+                    id={currentRoute._id}
+                    cardTitleOne={currentRoute.start_location}
+                    cardTitleTwo={currentRoute.end_location}
+                    to="to"
+                />
+            </Row>
                 <Row>
-                    <Card
-                        key="location"
-                        id={currentRoute._id}
-                        cardTitleOne={currentRoute.start_location}
-                        cardTitleTwo={currentRoute.end_location}
-                        to="to"
-                    />
+                <Col size="6"> 
+                <div>
+                    <h2 className="view">Route</h2>
+                    <img src={map} alt="route"></img>
+                </div>
+                </Col>
+                <Col size="6">
+                <div id="routeInstructions"> 
+                <h2 className="view">Route Instructions</h2>
+                {/* do an or in case this doenst load */}
+                    {routeInstructions.map((route, index) =>
+                        <li key={index}>
+                            {route.maneuver.instruction}
+                        </li>
+                    )}
+                </div>
+                
+                </Col>
                 </Row>
                 <Row>
-                    <Col size="6">
-                        <div>
-                            <h2 className="view">Route</h2>
-                            <div id="displayMap"></div>
-                        </div>
-                    </Col>
-                    <Col size="6">
-                        <div id="routeInstructions">
-                            <h2 className="view">Route Instructions</h2>
-                            {/* do an or in case this doenst load */}
-                            {!routeInstructions.length ? (<h1>Search a Route</h1>) : routeInstructions.map((route, index) =>
-                                <li key={index}>
-                                    {route.maneuver.instruction}
-                                </li>
-                            )}
-                        </div>
-
-                    </Col>
-                </Row>
-                <Row>
-                    <h2 className="view">8 Day Forecast</h2>
-                    {!weather.length ? (<p>No Data Available</p>) : weather.map((temp) =>
+                <h2 className="view">8 Day Forecast</h2>
+                    {weather.map((temp) =>
                         <Col size="3" key={temp.dt}>
                             <Card
                                 key={temp.dt}
