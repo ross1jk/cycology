7b2edf06abd5a47bb22c8ed4769236ea85b60b82
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-07-28 10:40:29 -0400

    fixed models

--- models/route.js
+++ models/route.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const routeSchema = new Schema({
   start_location: { type: String, required: true },
   end_location: { type: String, required: true},
+  start_coordinates:{ type: String, required: true}, 
+  end_coordinates:{ type: String, required: true}, 
   review: { type: String, default: "" },
   rating: { type: Number, default: 0 }
 });
